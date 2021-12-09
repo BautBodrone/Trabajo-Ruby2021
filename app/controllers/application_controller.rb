d199bb4d362d23c5cec8d632457958d79867b89e
BautBodrone <51447970+BautBodrone@users.noreply.github.com>
2021-12-09 15:19:16 -0300

    FIX CANCANCAN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
     appointments_path
   end
 
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   respond_to do |format|
-  #     format.json { head :forbidden }
-  #     format.html { redirect_to root_path, notice: exception.message }
-  #   end
-  # end
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, notice: exception.message }
+    end
+  end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,8 @@ class Ability
 
   def initialize(user)
     can :manage, :all if user.role == "admin"
-    can :read, :professionals if user.role == "assistant"
     can :manage, :appointments if user.role == "assistant"
+    can :read, :all if user.role == "assistant"
     can :read, :all if user.role == "watcher"
   end
 end
